c0eb17db6d3a110c8f65c48e994d852abbfcd9e1
SHIV PRAKASH YADAV <shiv.yadav@lumiq.ai>
2022-05-26 10:32:10 +0000

    Added new file init_orders_pivoted.sql ( Find amount paid  by different methods for each order id )

---
+++ models/marts/core/init_orders_pivoted.sql
@@ -0,0 +1,20 @@
+{% set payments_methods = ['credit_card','coupon','bank_transfer','git_cards'] %}
+
+with payment as(
+    select * from {{ref('stg_payments')}}
+),
+
+pivoted as (
+    select
+        order_id,
+        {% for method in payments_methods %}
+        sum(case when payment_method='{{method}}' then amount else 0 end) as {{method}}_transfer
+        {% if not loop.last %}
+        ,
+        {%endif%}
+        {%endfor%}
+        from payment
+        where status='success'
+        group by 1
+)
+select * from pivoted
--- models/staging/stripe/stg_payments.sql
+++ models/staging/stripe/stg_payments.sql
@@ -2,7 +2,7 @@ with payments as(
     select
     id as payment_id,
     orderid as order_id,
-    Amount as Amount,status
+    Amount as Amount,status,paymentmethod as payment_method
     from {{ source('payment', 'payment') }}
 )
 select * from payments
